37b571adcc5e83362126f0a065167edc116e9130
apthos <david00lara@gmail.com>
2020-06-26 12:34:04 -0400

    Added movie details view

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by David Lara on 6/26/20.
+//  Copyright © 2020 davol00. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+@property (nonatomic, strong) NSDictionary *movie;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
